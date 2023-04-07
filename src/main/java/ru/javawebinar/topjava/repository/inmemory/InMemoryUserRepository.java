173b46434bee8066861c3e28035049df722cc1a8
MaximKiel <loup.du.nord@yandex.ru>
2023-04-07 10:29:53 +0300

    Implement InMemoryMealRepository.java Add field userId into Meal.java Add *.sorted into the method getAll() of InMemoryUserRepository.java Moved MealTo.java into **.to package Edit MealServlet.java with new methods of InMemoryMealRepository.java

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -3,19 +3,13 @@ package ru.javawebinar.topjava.repository.inmemory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
-import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.BiFunction;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 @Repository
@@ -51,7 +45,7 @@ public class InMemoryUserRepository implements UserRepository {
     public List<User> getAll() {
         log.info("getAll");
         return repository.values().stream()
-                .sorted((o1, o2) -> o1.getName().equals(o2.getName()) ? o1.getEmail().compareTo(o2.getEmail()) : o1.getName().compareTo(o2.getName()))
+                .sorted((o1, o2) -> !o1.getName().equals(o2.getName()) ? o1.getName().compareTo(o2.getName()) : o1.getEmail().compareTo(o2.getEmail()))
                 .collect(Collectors.toList());
     }
 
