674b34c2207104d4d2f228328314e2c152d94f93
MaximKiel <loup.du.nord@yandex.ru>
2023-06-22 08:45:36 +0300

    Implement methods get, delete, update into MealRestControllerTest.java

--- src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
@@ -1,10 +1,22 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.javawebinar.topjava.MealTestData;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 import ru.javawebinar.topjava.web.AbstractControllerTest;
+import ru.javawebinar.topjava.web.json.JsonUtil;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.*;
 
 class MealRestControllerTest  extends AbstractControllerTest {
 
@@ -13,4 +25,38 @@ class MealRestControllerTest  extends AbstractControllerTest {
     @Autowired
     private MealService mealService;
 
+    @Test
+    void get() throws Exception {
+        perform(MockMvcRequestBuilders.get(MEAL_URL + MEAL1_ID))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(MEAL_MATCHER.contentJson(mealService.get(MEAL1_ID, USER_ID)));
+    }
+
+    @Test
+    void delete() throws Exception {
+        perform(MockMvcRequestBuilders.delete(MEAL_URL + MEAL1_ID))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+        assertThrows(NotFoundException.class, () -> mealService.get(MEAL1_ID, USER_ID));
+    }
+
+    @Test
+    void update() throws Exception {
+        Meal mealUpdated = MealTestData.getUpdated();
+        perform(MockMvcRequestBuilders.put(MEAL_URL + MEAL1_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(mealUpdated)))
+                .andExpect(status().isNoContent());
+        MEAL_MATCHER.assertMatch(mealService.get(MEAL1_ID, USER_ID), mealUpdated);
+    }
+
+//    @Test
+//    void getAll() throws Exception {
+//        perform(MockMvcRequestBuilders.get(MEAL_URL))
+//                .andExpect(status().isOk())
+//                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+//                .andExpect(MEAL_MATCHER.contentJson(mealService.getAll(USER_ID)));
+//    }
 }
